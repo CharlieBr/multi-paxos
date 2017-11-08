4941d84500d78d42562aa949b058d40e3d11a92a
leszek <leszekplaczkiewicz@gmail.com>
2017-11-08 19:45:11 +0100

    Fix building issues.

--- node/src/test/java/iosr/multipaxos/node/NodeApplicationTests.java
+++ node/src/test/java/iosr/multipaxos/node/NodeApplicationTests.java
@@ -3,10 +3,12 @@ package iosr.multipaxos.node;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = {"id=1", "server.port=8080", "members=localhost:8081"})
 public class NodeApplicationTests {
 
 	@Test
